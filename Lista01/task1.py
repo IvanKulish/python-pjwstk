ae995031307a44ba1948fb1863d73cd6cdc3a2dd
IvanKulish <127194451+IvanKulish@users.noreply.github.com>
2023-04-18 18:12:36 +0200

    Add files via upload

---
+++ Lista01/task1.py
@@ -0,0 +1,5 @@
+"""
+Zadanie 1
+"""
+print("Robert Lewandowsk", " Kylian Mbappe", " Erling Haaland", " Karim Benzema", " Mohamed Salah", " Edouard Mendy",
+      " Cristiano Ronaldo", " Kevin De Bruyne", " Lionel Messi", " Ciro Immobile")
