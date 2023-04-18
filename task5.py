ac6d5e4fcdf163a437112d0b7cf4de02a79a2d54
IvanKulish <127194451+IvanKulish@users.noreply.github.com>
2023-04-18 19:11:21 +0300

    Delete task5.py

--- task5.py
+++
@@ -1,7 +0,0 @@
-"""
-Zadanie 5
-"""
-nicki = {"Robert Lewandowski": 34, "Kylian Mbappe": 23, "Erling Haaland": 22, "Karim Benzema": 35,
-         "Mohamed Salah": 30, "Edouard Mendy": 31, "Cristiano Ronaldo": 37, "Kevin De Bruyne": 31,
-         "Lionel Messi": 35, "Ciro Immobile": 33}
-print(nicki)
