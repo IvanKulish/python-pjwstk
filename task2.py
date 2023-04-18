8a0dd1512a8aa0c894d375dac597f6592e9c3bd6
IvanKulish <127194451+IvanKulish@users.noreply.github.com>
2023-04-18 19:11:02 +0300

    Delete task2.py

--- task2.py
+++
@@ -1,5 +0,0 @@
-"""
-Zadanie 2
-"""
-print("Robert Lewandowsk", "Kylian Mbappe", "Erling Haaland", "Karim Benzema", "Mohamed Salah", "Edouard Mendy",
-      "Cristiano Ronaldo", "Kevin De Bruyne", "Lionel Messi", "Ciro Immobile", sep="\n")
