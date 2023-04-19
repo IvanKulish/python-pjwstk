585495511f48ec9116843a2bffd0369ce11237c3
IvanKulish <127194451+IvanKulish@users.noreply.github.com>
2023-04-19 11:36:16 +0200

    Add files via upload

---
+++ Lista01/task6.py
@@ -0,0 +1,15 @@
+"""
+Zadanie 6
+"""
+footballer = {"Robert Lewandowski":{"2023-04-19" : 34}, 
+        "Kylian Mbappe":{"2023-04-19" : 23} , 
+        "Erling Haaland": {"2023-04-19" : 22}, 
+        "Karim Benzema": {"2023-04-19" : 35},
+         "Mohamed Salah": {"2023-04-19" : 30}, 
+         "Edouard Mendy": {"2023-04-19" : 31}, 
+         "Cristiano Ronaldo": {"2023-04-19" : 37}, 
+         "Kevin De Bruyne": {"2023-04-19" : 31},
+         "Lionel Messi": {"2023-04-19" : 35}, 
+         "Ciro Immobile": {"2023-04-19" : 33}
+         }
+print(footballer)
